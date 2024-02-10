a05b17512e1c14bd6259e8962ee1bde7dcffe209
Aymeric Ratinaud <aymeric.ratinaud@sfr.fr>
2024-02-10 18:24:03 +0100

    NoteEntityListener

--- src/DoctrineListener/NoteEntityListener.php
+++ src/DoctrineListener/NoteEntityListener.php
@@ -1,19 +1,3 @@
-# Api platform Doctrine
-
-1. If not already done, [install Docker Compose](https://docs.docker.com/compose/install/) (v2.10+)
-2. Run `docker compose build --no-cache` to build fresh images
-3. Run `docker compose up --pull always -d --wait` to start the project
-4. Open `https://localhost` in your favorite web browser and [accept the auto-generated TLS certificate](https://stackoverflow.com/a/15076602/1352334)
-5. Run `docker compose down --remove-orphans` to stop the Docker containers.
-
-## Load fixtures
-
-`make sh` to enter the container
-`php bin/console doctrine:fixtures:load`
-
-## Note Listener
-
-```php
 <?php
 
 namespace App\DoctrineListener;
@@ -45,5 +29,3 @@ class NoteEntityListener
         $note->setLog('This is from preUpdate');
     }
 }
-
-```
---
+++ src/Entity/Note.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NoteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NoteRepository::class)]
+class Note
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $log = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getLog(): ?string
+    {
+        return $this->log;
+    }
+
+    public function setLog(string $log): static
+    {
+        $this->log = $log;
+
+        return $this;
+    }
+}
