4574e2f1c3974a80a6bc3f1f9086736205872094
Aymeric Ratinaud <aymeric.ratinaud@sfr.fr>
2024-03-06 15:08:17 +0100

    remove dead code

--- src/DoctrineListener/NoteEntityListener.php
+++ src/DoctrineListener/NoteEntityListener.php
@@ -6,7 +6,6 @@ use App\Entity\Conference;
 use App\Entity\Note;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
-use Symfony\Component\String\Slugger\SluggerInterface;
 use Doctrine\ORM\Events;
 
 
@@ -14,11 +13,6 @@ use Doctrine\ORM\Events;
 #[AsEntityListener(event: Events::preUpdate, entity: Note::class)]
 class NoteEntityListener
 {
-    public function __construct(
-        private SluggerInterface $slugger,
-    ) {
-    }
-
     public function prePersist(Note $note, LifecycleEventArgs $event)
     {
         $note->setLog('This is from prePersist');
